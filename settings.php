7cdddcb03b2bcf31ff514255522d109a01949c6d
abdoecomerce <abdoelprince12345@gmail.com>
2022-03-06 19:40:41 +0200

    starting the profile page

--- settings.php
+++ settings.php
@@ -1,3 +1,45 @@
 <!-- => update name
     username , country , user photo , eamil , cover Phote , password , 
 -->
+
+
+<?php
+
+ob_start();
+session_start();
+
+$pageTitle = "Homepage";
+include './init.php';
+?>
+
+<main class="setting_page">
+    <div class="container">
+        <div class="row">
+            <form>
+                <div class="col-lg-6">
+                    <input type="text" placeholder="update your name">
+                </div>
+                <div class="col-lg-6">
+                    <input type="text" placeholder="update your name">
+                </div>
+            </form>
+        </div>
+    </div>
+
+</main>
+
+
+
+
+
+
+
+
+
+
+<?php
+
+include './includes/templates/footer.php';
+
+ob_flush();
+?>
