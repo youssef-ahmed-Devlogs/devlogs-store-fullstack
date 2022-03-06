ddcd0b9abeecd503ba182ad846b0b1f3669f2d69
youssefahmeddevlogs <youssefahmedsup@gmail.com>
2022-03-06 17:39:42 +0200

    Updates

--- admin/slider.php
+++ admin/slider.php
@@ -2,17 +2,32 @@
 
 include 'init.php';
 
-$action = isset($_GET['action']) && !empty($_GET['action']) $_GET['action'] : "manage";
+$action = isset($_GET['action']) && !empty($_GET['action']) ? $_GET['action'] : "manage";
 
-//if() {
-//
-//}
+if($action == 'manage') {
+    echo "Manage";
+} elseif ($action == 'add') {
+
 ?>
 
-    <form action="">
+    <form action="?action=insert" method="POST" enctype="multipart/form-data">
         <label for="slide_img" class="btn btn-primary">upload</label>
         <input type="file" name="slide_img" id="slide_img">
+
+        <button>Add</button>
     </form>
+
+<?php
+
+} elseif ($action == 'insert') {
+
+    echo '<pre>';
+    print_r($_FILES['slide_img']);
+    echo '</pre>';
+
+}
+
+?>
 
 <?php
 include 'includes/templates/footer.php';
